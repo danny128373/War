03a621f6ce8171b8dcf4961173acff685ca495c5
Daniel Meza <daniel.meza29@yahoo.com>
2020-07-20 13:34:50 -0500

    Initial commit

---
+++ index.py
@@ -0,0 +1,36 @@
+# try:
+#     for i in ['a', 'b', 'c']:
+#         print(i**2)
+# except TypeError:
+#     print("There's a TypeError in your code bruh")
+# finally:
+#     print("It's all good, debug and try again!")
+
+# x = 5
+# y = 0
+
+# try:
+#     z = x/y
+#     print(z)
+# except ZeroDivisionError:
+#     print("Maybe you mixed up the numerator and the denominator")
+# finally:
+#     print("Division is fun!")
+
+
+# def ask():
+#     while True:
+#         try:
+#             num = int(input("Please provide an integer: "))
+#             print(f"The square of the number you entered is {num ** 2} ")
+#         except:
+#             print("Hey douchebag, you didn't enter a number")
+#             continue
+#         else:
+#             print("Thank you, douchebag.")
+#             break
+#         finally:
+#             print("Fuck")
+
+
+# ask()
