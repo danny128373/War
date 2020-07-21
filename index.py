ae8cbd030cb836e53de726caec62f91adbe01bce
Daniel Meza <daniel.meza29@yahoo.com>
2020-07-21 13:50:29 -0500

    Finished beginning of each round which is to check if we have a winner

--- index.py
+++ index.py
@@ -3,24 +3,35 @@ from deck import Deck
 from player import Player
 import settings
 
-two_hearts = Card("Hearts", "Two")
-three_clubs = Card("Clubs", "Three")
+player_one = Player("Jose")
+player_two = Player("Daniel")
+
 new_deck = Deck()
-
-# print(settings.values[two_hearts.rank])
-# print(settings.values[three_clubs.rank])
-# print(new_deck.all_cards)
-print(new_deck.all_cards[-1])
 new_deck.shuffle()
-my_card = new_deck.deal_one()
-print(my_card)
-print(len(new_deck.all_cards))
-new_player = Player("Jose")
-print(new_player)
-new_player.add_cards(my_card)
-new_player.add_cards(my_card)
-new_player.add_cards(my_card)
-print(new_player)
 
-for card in new_player.all_cards:
-    print(f"{new_player.name} has the card: {card}")
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+round_num = 0
+# while game_on
+#     while at war
+while game_on:
+    round_num += 1
+    print(f"Round {round_num}")
+
+    if len(player_one.all_cards) == 0:
+        print("Player One out of cards! Player Two Wins!")
+        game_on = False
+        break
+
+    if len(player_one.all_cards) == 0:
+        print("Player Two out of cards! Player One Wins!")
+        game_on = False
+        break
+
+    # Start a new round
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
