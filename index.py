3c527c4b61e474cdb12fabeb07623ee86ba7b4fd
Daniel Meza <daniel.meza29@yahoo.com>
2020-07-21 15:45:48 -0500

    Finished the card game war

--- index.py
+++ index.py
@@ -14,8 +14,9 @@ for x in range(26):
     player_two.add_cards(new_deck.deal_one())
 
 round_num = 0
-# while game_on
-#     while at war
+
+game_on = True
+
 while game_on:
     round_num += 1
     print(f"Round {round_num}")
@@ -33,5 +34,39 @@ while game_on:
     # Start a new round
     player_one_cards = []
     player_one_cards.append(player_one.remove_one())
+
     player_two_cards = []
     player_two_cards.append(player_two.remove_one())
+
+    at_war = True
+
+    while at_war:
+
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+            at_war = False
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+            at_war = False
+
+        else:
+            print("WAR!")
+            if len(player_one.all_cards) < 5:
+                print("Player One unable to declare war.")
+                print("Player Two Wins!")
+                game_on = False
+                break
+
+            elif len(player_two.all_cards) < 5:
+                print("Player Two unable to declare war.")
+                print("Player One Wins!")
+                game_on = False
+                break
+
+            else:
+                for num in range(5):
+                    player_one_cards.append(player_one.remove_one)
+                    player_two_cards.append(player_two.remove_one)
